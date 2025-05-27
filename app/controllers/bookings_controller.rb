eeb5c6cbf527f51eba666db83f5e29f049c30dc1
charlie gardere <charlie.gardere@gmail.com>
2025-05-27 12:56:24 +0200

    booking route nesté dans flat, pour faire le booking depuis la show de flat, show de flat commencé,et form de booking/new  operationnel dans la show de flat

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,41 +1,41 @@
-class BookingsController < ApplicationController
+  class BookingsController < ApplicationController
 
-  def show
-  end
-
-  def new
-    @booking = Booking.new()
-  end
-
-  def create
-    @flat = Flat.last # a terme il sera choisi automatiquement
-    @booking = Booking.new(booking_params)
-    @booking.user = current_user
-    @booking.flat = @flat # ca ca va bouger aussi
-
-    if @booking.save
-      redirect_to booking_path(@booking)
-    else
-      render :new, status: :unprocessable_entity
+    def show
     end
+
+    def new
+      @booking = Booking.new()
+    end
+
+    def create
+      @flat = Flat.find(params[:flat_id])
+      @booking = Booking.new(booking_params)
+      @booking.user = current_user
+      @booking.flat = @flat # ca ca va bouger aussi
+
+      if @booking.save
+        redirect_to flat_path(@flat)
+      else
+        render "flats/show", flat: @flat ,status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @booking = Booking.find(params[:id])
+      @booking.update(booking_params)
+      redirect_to bookings_path
+    end
+
+    def delete
+      @booking = Booking.find(params[:id])
+      @booking.destroy
+      redirect_to bookings_path, status: :see_other
+    end
+
+    private
+
+    def booking_params
+      params.require(:booking).permit(:entry_date, :out_date, :guest)
+    end
+
   end
-
-  def update
-    @booking = Booking.find(params[:id])
-    @booking.update(booking_params)
-    redirect_to bookings_path
-  end
-
-  def delete
-    @booking = Booking.find(params[:id])
-    @booking.destroy
-    redirect_to bookings_path, status: :see_other
-  end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:entry_date, :out_date, :guest)
-  end
-
-end
