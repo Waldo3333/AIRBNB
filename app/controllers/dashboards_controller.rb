798ce09c09a9c0fc41f64d79395c23bf7a7c6cf1
florent.bedubourg@supdevinci-edu.fr <florent.bedubourg@supdevinci-edu.fr>
2025-05-27 16:24:38 +0200

    dashboards: booking/confirm booking flats, redirect new flats

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,9 @@
 class DashboardsController < ApplicationController
   def index
     @bookings = current_user.bookings
+    @bookings_confirmed = @bookings.confirmed
+    @bookings_unconfirmed = @bookings.unconfirmed
+    @flats = current_user.flats
   end
+  
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :flat
   belongs_to :user
+
+  scope :confirmed, -> { where(confirmed: true)}
+  scope :unconfirmed, -> { where.not(confirmed: true)}
 end
