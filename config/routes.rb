15dcbed149cfd9d9c42009d8de93cf88cbdea8ca
florent.bedubourg@supdevinci-edu.fr <florent.bedubourg@supdevinci-edu.fr>
2025-05-27 12:10:46 +0200

    booking update qui marche vraiment sûr

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-<<<<<<< HEAD
+
   resources :flats, only: [:show, :index]
-  resources :booking
-
-=======
->>>>>>> dc68ec505436bec320431d4a577649b8fefbaf35
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
   resources :bookings
+
 end
